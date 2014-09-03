46fad828fed8b14375e6988e2ac4aa6c50700df2
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-09-03 15:22:39 -0700

    first commit

---
+++ person.js
@@ -0,0 +1,21 @@
+var	person = { names: "Andrea",
+age: 4,
+favoritecolor: "purple",
+hobby: "cooking"
+}; 
+
+/* console.log (person["names"]);
+console.log (person["age"]);
+
+person["age"]=12
+person["names"]= "football"
+
+console.log (person["names"]);
+console.log (person["age"]);*/
+
+//list variables
+
+console.log ("My name is\ " + person["names"] + ". " +
+"I will be\ " + person["age"] + "\ years old." + " " +
+"I love\ "  + person["hobby"] + ". ");
+
---
+++ stuff.js
@@ -0,0 +1,14 @@
+
+//list variables
+
+var name = ('My name Andrea.\n ');
+var age = ('I will be 34 years old\ ');
+var birthday = ('on the 20th.\n ');
+var hometown =  ('I grew up in Salt Lake City.\n ');
+var hobby = ('I love video games.\n ');
+var young = ('When I was young I liked\ ');
+var hobby2 = ('softball.');
+
+//print variables
+
+console.log (name + age + birthday + hometown + hobby + young + hobby2);
