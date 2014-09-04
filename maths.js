14c5412730f0539fc9d19e260290064ee96d0809
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-09-03 17:02:15 -0700

    math

---
+++ maths.js
@@ -0,0 +1,14 @@
+
+var mathStuffs = function (a, b) {
+	var sum = a + b;
+	console.log(a + "+" + b + "=" + sum);
+	var mult = a * b;
+	console.log(a + "*" + b + "=" + mult);
+	var div = a / b;
+	console.log(a + "/" + b + "=" + div);
+	var sub = a - b;
+	console.log(a + "-" + b + "=" + sub);
+};
+
+
+mathStuffs(2,5)
--- person.js
+++ person.js
@@ -1,10 +1,11 @@
-var	person = { names: "Andrea",
+var	me1 = { names: "Andrea",
 age: 4,
 favoritecolor: "purple",
-hobby: "cooking"
+hobby: "cooking",
+firstWord: "ma"
 }; 
 
-/* console.log (person["names"]);
+/* console.log (me1["names"]);
 console.log (person["age"]);
 
 person["age"]=12
@@ -15,7 +16,12 @@ console.log (person["age"]);*/
 
 //list variables
 
-console.log ("My name is\ " + person["names"] + ". " +
-"I will be\ " + person["age"] + "\ years old." + " " +
-"I love\ "  + person["hobby"] + ". ");
+console.log ("My name is\ " + me1["names"] + ". " +
+"I will be\ " + me1["age"] + "\ years old." + " " +
+"I love\ "  + me1["hobby"] + ". ");
+		
 
+var speak = function (person, firstWord) {
+	console.log (person["names"] + " says \" " + firstWord + " \" for the first time")
+}
+speak(me1, me1["firstWord"])
