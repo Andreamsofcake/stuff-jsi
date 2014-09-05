cb34799068acfb83e1b5e520fc689df3ada9324e
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-09-05 15:33:41 -0700

    new examples

---
+++ example.js
@@ -0,0 +1,26 @@
+//functions
+var blank = function function_name (argument) {
+	// body...
+};
+
+function_name(argument);
+
+//array
+var array['','',''];
+array.splice(0,0, '');
+
+//print
+console.log('print this stuff');
+
+//object
+var object = {
+	key1 = 'name',
+	key2 = 'amount'
+};
+
+//maths
+Math.sqrt () //square root
+Math.E () //nat. log
+
+//string
+n.toString()
---
+++ tdd.js
@@ -0,0 +1,24 @@
+// var result = difference(5, 2);
+// expect(result).to.eql(3);
+
+//calculate intrust function
+
+/**
+*@param {number} principal Number of dollars barrowed
+*@param {number} interest Interest rate, between 0 and 1
+*@param {number} time Number of Year
+*@return {number} The interest (not including the principal, obbviously)
+**/
+
+describe('calculateInterest()', function() {
+  it('calculates interest of $10 barrowed at 4% for 1 year', function() {
+    // principal
+    // interest rate
+    // amount of time
+    // it returns inters
+  });
+  var interest = calculateInterest(10, 0.04, 1)
+
+	});
+//expect inerest of 0.41 and total amount of 10.41
+		expect(interest).to,eql(0.41);
